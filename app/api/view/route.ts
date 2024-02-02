9985f558cccb379c1490384a461db389b95c2dfb
Andre Neves <andrerfneves@protonmail.com>
2024-02-01 20:33:14 -0800

    chore: updates

--- app/api/view/route.ts
+++ app/api/view/route.ts
@@ -37,7 +37,7 @@ export async function GET(req: NextRequest) {
   }
 
   if (url.searchParams.get("incr") != null) {
-    const views = await redis.hincrby("views", id, 100);
+    const views = await redis.hincrby("views", id, 1007);
     return NextResponse.json({
       ...post,
       views,
